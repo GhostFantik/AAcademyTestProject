3b146c7f3d79798ba165deb1f01d8ac8a7b08cca
GhostFantik <ghostfantik@gmail.com>
2021-04-15 11:45:55 +0300

    Resources model was created

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Resources(models.Model):
+    title = models.CharField('Название', max_length=255)
+    amount = models.FloatField('Количество')
+    unit = models.CharField('Единица измерения', max_length=50)
+    price = models.FloatField('Цена')
+    date = models.DateTimeField('Дата последнего поступления')
